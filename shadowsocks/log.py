15af789af7213f5deab2295b696b224e48ee692d
liming <827182486@qq.com>
2023-03-07 22:15:53 +0800

    添加日志模块

---
+++ shadowsocks/log.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+import logging.handlers
+
+
+def get_logger(name='my_logger', filename='test.log'):
+    # 创建日志记录器
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # 创建控制台处理程序
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    # 创建文件处理程序
+    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=1024, backupCount=7)
+    file_handler.setLevel(logging.INFO)
+
+    # 创建格式器
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # 将格式器添加到处理程序中
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # 将处理程序添加到日志记录器中
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    return logger
